71081dc549c7c9f8f31a72ba1d8e8e7c3e42e500
Younus Bipul <bipulyounus@gamil.com>
2020-06-10 01:38:37 +0600

    deleting profile pics

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
     mongoDbUrl :'mongodb://localhost:27017/blog'
+    // mongoDbUrl: 'mongodb+srv://blog-admin:blog-admin1234@firstcluster-mttb8.mongodb.net/blog?retryWrites=true&w=majority'
 }
